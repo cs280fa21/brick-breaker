80a1b28b2874bfea12d475f93ada160c3e770b61
Ali Madooei <alimadooei@gmail.com>
2021-09-11 23:40:37 -0400

    Step 4 Canvas

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.rect(20, 40, 50, 50); // x, y, width, height
+ctx.fillStyle = "#FF0000";
+ctx.fill();
+ctx.closePath();
